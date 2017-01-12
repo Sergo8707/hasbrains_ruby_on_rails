e089f74b7b4cebda1fec487532d2a5b23ecb65c7
Sergo8707 <sergo8707@gmail.com>
2017-01-13 01:21:22 +0200

    Model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
-  attr_accessible :price, :name, :real, :weight, :description
+
+  validates :price, numericality: {greater_than: 0, allow_nil: true}
+  validates :name, :description, presence: true
+
+  private
+
+  def item_params
+    params.require(:item).permit(:price, :name, :real, :weight, :description)
+  end
 end
