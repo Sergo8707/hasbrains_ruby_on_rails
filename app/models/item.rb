9a8e059b71a096f6e89d6fee5b050562581d5ac5
Sergo8707 <sergo8707@gmail.com>
2017-01-13 12:00:39 +0200

    Model callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,13 @@ class Item < ApplicationRecord
   validates :price, numericality: {greater_than: 0, allow_nil: true}
   validates :name, :description, presence: true
 
+
+  after_initialize { puts 'initialized'}
+  after_save { puts 'saved'}
+  after_create { puts 'created'}
+  after_update { puts 'updated'}
+  after_destroy { puts 'destroyed'}
+
   private
 
   def item_params
